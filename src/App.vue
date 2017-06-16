ea9349d5530fd65b42e1868167d4c5ce44511d9d
Ben <benstep222@gmail.com>
2017-06-16 22:06:03 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<style type="text/scss" lang="scss" scoped>
+</style>
+
+<script type="text/ecmascript-6">
+  export default {
+    name: 'app',
+    data () {
+      return {}
+    },
+    components: {}
+  }
+</script>
